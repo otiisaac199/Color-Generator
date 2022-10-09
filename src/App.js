05ee38c8aa9e625b1211e47efc9e5ca2a758fad3
otiisaac199 <otichinazaisaac19@gmail.com>
2022-10-09 19:23:06 +0100

    modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SingleColor from "./SingleColor";
 // import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>;
+  return <h2>color generator</h2>;
 }
 
 export default App;
