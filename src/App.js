bc34f26c3f94e4aea25305260917826ed4af25d2
otiisaac199 <otichinazaisaac19@gmail.com>
2022-10-13 12:23:11 +0100

    features: mapped through the list

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
-      console.log(colors);
+      setList(colors);
     } catch (error) {
       setError(true);
       console.log(error);
@@ -36,8 +36,11 @@ function App() {
           </button>
         </form>
       </section>
-      <section className="color">
-        <h4>list goes here</h4>
+      <section className="colors">
+        {list.map((color, index) => {
+          console.log(color);
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
       </section>
     </>
   );
